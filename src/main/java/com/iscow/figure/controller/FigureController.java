3f46ce0ad8a01eabbcae5a2fc5baac508c7ee9d5
guanhao <guanhao@zhongwang.com>
2020-12-28 11:34:55 +0800

    Initial commit

---
+++ src/main/java/com/iscow/figure/controller/FigureController.java
@@ -0,0 +1,15 @@
+package com.iscow.figure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/is-cow")
+public class FigureController {
+    @GetMapping("/figure/{num}")
+    public String figure(@PathVariable("num") Integer num){
+        return num + " figured";
+    }
+}
